445b4b3e23b5d55eacc8627d032134853cd4fe60
Marcos <marcsalexandr@gmail.com>
2024-04-12 13:44:01 -0300

    first commit

---
+++ src/main/kotlin/one/digitalinnovation/avengers/avengers/api/Application.kt
@@ -0,0 +1,11 @@
+package one.digitalinnovation.avengers.avengers.api
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Application
+
+fun main(args: Array<String>) {
+	runApplication<Application>(*args)
+}
